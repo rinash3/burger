85e80b20f9bdc94c036d62fda672527dc92cba2c
Rina Razumov <rinash3@gmail.com>
2019-07-12 23:36:46 -0400

    back to old code

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,23 +1,4 @@
 $(function() {
-  $(".devour-it").on("click", function(event) {
-    var id = $(this).data("id");
-  
-    var newDevourState = {
-      devour: true
-    };
-
-    // Send the PUT request.
-    $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: newDevourState
-    }).then(
-      function() {
-        console.log("changed devour to", newDevourState);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
 
   $(".create-form").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
@@ -40,4 +21,26 @@ $(function() {
       }
     );
   });
+
+  $("eatbutton").on("click", function(event) {
+    var id = $(this).data("id");
+  
+    var newDevourState = {
+      devour: true
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevourState
+    }).then(
+      function() {
+        console.log("changed devour to", newDevourState);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+ 
 })
